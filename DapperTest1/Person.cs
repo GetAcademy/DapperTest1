3fb81f59e7eacca2113a4daa5ba35574a939ab20
GET Academy <terje@kolderup.net>
2019-05-29 15:12:38 +0200

    x

---
+++ DapperTest1/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperTest1
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public int Fødselsår { get; set; }
+    }
+}
---
+++ DapperTest1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+namespace DapperTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=getit;Integrated Security=True;";
+            var conn = new SqlConnection(connStr);
+
+            var readMange = @"SELECT Id, Fornavn, Etternavn, Fødselsår
+                              FROM Person";
+
+            // DECLARE @Id int = 1001;
+            var readEn = @"SELECT Id, Fornavn, Etternavn, Fødselsår
+                           FROM Person
+                           WHERE Id = @Id";
+
+            var create = @"INSERT INTO Person(Fornavn, Etternavn, Fødselsår)
+                        VALUES('Terje', 'Kolderup', 1975)";
+
+            var delete = @"DELETE FROM Person WHERE Id = @Id";
+
+            var update = @"UPDATE Person
+                           SET Etternavn = 'Pettersen', Fornavn = 'Petter'
+                           WHERE Id = @Id";
+
+            conn.Query<Person>()
+        }
+    }
+}
